6a1c4c6f9e8d662b63ada3b10b54480101984935
Leonardo Carvalho <leorrc1@gmail.com>
2023-05-09 18:26:20 -0300

    add new card project

--- src/services/global/api.ts
+++ src/services/global/api.ts
@@ -2,23 +2,23 @@ import axios from 'axios'
 import { createRepoKey } from './keys'
 
 const URL = 'https://raw.githubusercontent.com/Leorrc'
-const repo_names = ['juliana-interior-designer', 'site-atc', 'SiteMaria','pokedex', 'my-page-v2']
+const repo_names = ['gengar-control', 'juliana-interior-designer', 'site-atc', 'SiteMaria', 'pokedex', 'my-page-v2']
 
 async function getRepos() {
   createRepoKey()
-    try {
-      const repos_Promises = repo_names.map(
-        async repo => await axios.get(`${URL}/${repo}/master/project-info.json`)
-      )
+  try {
+    const repos_Promises = repo_names.map(
+      async repo => await axios.get(`${URL}/${repo}/master/project-info.json`)
+    )
 
-      const repos_data = await Promise.all(repos_Promises).then(responses =>
-        responses.map(response => response.data)
-      )
+    const repos_data = await Promise.all(repos_Promises).then(responses =>
+      responses.map(response => response.data)
+    )
 
-      return repos_data
-      
-    } catch (e) {}
-  }
+    return repos_data
+
+  } catch (e) { }
+}
 
 export const api = {
   getRepos
